40d17937ae078559497447f25f158ebd0b14a080
Bertrand Fritsch <bertrand.fritsch.in@gmail.com>
2022-05-08 20:52:23 +0200

    Walk through all the files to get the external dependencies

--- src/get-external-imports.ts
+++ src/get-external-imports.ts
@@ -1,14 +1,31 @@
+import path from 'path';
+import fs from 'fs';
 import { initializeRootDirectory } from './helpers';
 import { walkModuleDependencies } from './lib/walkModuleDependencies';
+import { Command } from 'commander';
 
-initializeRootDirectory(process.argv[2]);
+const program = new Command();
 
-console.log(JSON.stringify(getExternalDependencyImports(process.argv[2]), null, 2));
+program.name('get-external-imports')
+  .description('Clean unused dependency of a set of typescript files')
+  .version('0.0.2');
 
-function getExternalDependencyImports(filename: string) {
+program.option('-r, --recursive', 'whether the internal dependencies have to be processed recursively', false)
+  .argument('<input source file> | <input json file>');
+program.parse();
+
+const sourceFiles = path.extname(program.args[0]) === '.json'
+  ? JSON.parse(fs.readFileSync(program.args[0], 'utf-8'))
+  : [program.args[0]];
+
+initializeRootDirectory(sourceFiles[0]);
+
+console.log(JSON.stringify(getExternalDependencyImports(), null, 2));
+
+function getExternalDependencyImports() {
   const modules = new Set<string>();
   
-  for (const { declarations } of walkModuleDependencies(filename)) {
+  for (const { declarations } of walkModuleDependencies(sourceFiles, program.opts().recursive)) {
     if (declarations.isExternalLibraryImport !== false) {
       modules.add(declarations.moduleSpecifier.replace(/((?:^@[\w-\.]+\/)?[\w-\.]+).*/, '$1'));
     }
